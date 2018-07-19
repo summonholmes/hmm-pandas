356d78d9e59f74479b1144ce512c9af17b76d485
Shane Kimble <shanekimble12@Hotmail.com>
2018-07-19 11:37:16 -0400

    More pythonic looping over observations

--- viterbi_pandas.py
+++ viterbi_pandas.py
@@ -5,7 +5,7 @@ from seaborn import light_palette
 
 ### Initialize tuples of conditions.  Observations are the input
 observations = ("Eating Pizza", "Browsing Reddit", "Drinking Mountain Dew",
-                "Eating Doritos", "Wearing Trenchcoat & Fedora")
+                "Eating Doritos", "Wearing Trenchcoat & Fedora") * 10
 hidden_states = ("Depressed", "Confident", "Tired", "Hungry",
                  "Thirsty")  # The confounding factors
 
@@ -40,12 +40,12 @@ start_probs = DataFrame(
 viterbi_df = start_probs.multiply(emit_prob_df[observations[0]], axis="index")
 
 ### Start dynammic programming
-for i in range(1, len(observations)):  # Offset from Probability 0
-    max_trans_prob_df = trans_prob_df.multiply(
-        viterbi_df.iloc[:, i - 1], axis="index").apply(
+for i, observation in enumerate(observations[1:]):
+    max_trans_prob_df = trans_prob_df.multiply(  # Offset by 1
+        viterbi_df.iloc[:, i], axis="index").apply(
             npmax, axis=0)  # Vectorize maximums for the previous column
     viterbi_df["Probability {}".format(  # Each column all in one go
-        i)] = max_trans_prob_df * emit_prob_df.loc[:, observations[i]]
+        i + 1)] = max_trans_prob_df * emit_prob_df.loc[:, observation]
 
 ### Provide the entire matrix with highest values darkest
 viterbi_traceback_df = viterbi_df.style.background_gradient(
@@ -54,22 +54,23 @@ viterbi_traceback_df = viterbi_df.style.background_gradient(
 ### At the last column, use the maximum value to begin traceback
 traceback_prob = [viterbi_df.iloc[:, -1].max()]
 dyn_prog_path = [viterbi_df.iloc[:, -1].idxmax()]  # And its index
-viterbi_traceback_df.highlight_max( # Highlight it
+viterbi_traceback_df.highlight_max(  # Highlight it
     color="red", subset=IndexSlice[[viterbi_df.columns[-1]]])
 
 ### Start traceback
-for i in range(len(observations) - 1, 0, -1):  # Countdown
+for i, observation in zip(  # Reverse enumerate with offset
+        range(len(observations) - 2, -1, -1), reversed(observations)):
     # Isolate the previous location that gives the current probability
-    traceback_loc = where(viterbi_df.iloc[:, i - 1] *
+    traceback_loc = where(viterbi_df.iloc[:, i] *
                           trans_prob_df.loc[:, dyn_prog_path[0]] *
-                          emit_prob_df.loc[dyn_prog_path[0], observations[i]]
-                          == traceback_prob[0])[0][0]
+                          emit_prob_df.loc[dyn_prog_path[0], observation] ==
+                          traceback_prob[0])[0][0]
     # Record the value and its state
-    traceback_prob.insert(0, viterbi_df.iloc[traceback_loc, i - 1])
+    traceback_prob.insert(0, viterbi_df.iloc[traceback_loc, i])
     dyn_prog_path.insert(0, viterbi_df.index[traceback_loc])
     viterbi_traceback_df = viterbi_traceback_df.applymap(
         lambda x: "background-color: red",  # Color the path red
-        subset=IndexSlice[[dyn_prog_path[0]], [viterbi_df.columns[i - 1]]])
+        subset=IndexSlice[[dyn_prog_path[0]], [viterbi_df.columns[i]]])
 
 ### Print dynammic programming matrix and traceback results
 print("The observations:", ", ".join(observations))
