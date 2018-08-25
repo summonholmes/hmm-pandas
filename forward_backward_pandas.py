f720874b9d0852b158b8496f694a9b99e1a15afd
summonholmes <summonholmes@10x-Orange-G.10x-Orange-G>
2018-08-25 15:58:55 -0400

    Enhance backward compatibility

--- forward_backward_pandas.py
+++ forward_backward_pandas.py
@@ -1,7 +1,7 @@
 from pandas import DataFrame
 from itertools import cycle
 
-### Initialize tuples of conditions.  Observations are the input
+# Initialize tuples of conditions.  Observations are the input
 observations = (  # Modify, add, remove with any key in emit_prob_df
     "Wearing Trenchcoat & Fedora", "Browsing Reddit", "Drinking Mountain Dew",
     "Eating Doritos", "Eating Pizza")
@@ -10,7 +10,7 @@ hidden_states = ("Depressed", "Confident", "Tired", "Hungry",
 colors = cycle(["red", "orange", "green", "blue", "purple"])  # Rainbow effect
 colors_dict = {}  # Each observation gets a color
 
-### Probability of transition
+# Probability of transition
 trans_prob_df = DataFrame(
     data={  # From depressed to confident, vice-versa, static, etc.
         "Depressed": (0.20, 0.25, 0.10, 0.20, 0.20),
@@ -20,29 +20,32 @@ trans_prob_df = DataFrame(
         "Thirsty": (0.20, 0.20, 0.25, 0.20, 0.10),
         "End": (0.01, 0.01, 0.01, 0.01, 0.01)
     },  # All should vertically sum to 1
+    columns=("Depressed", "Confident", "Tired", "Hungry", "Thirsty", "End"),
     index=hidden_states)
 
-### Probability of observation given the hidden state
+# Probability of observation given the hidden state
 emit_prob_df = DataFrame(
-    data={ # Highest chance of trenchcoat & fedora is when confident
+    data={  # Highest chance of trenchcoat & fedora is when confident
         "Eating Pizza": (0.20, 0.10, 0.10, 0.35, 0.20),
         "Browsing Reddit": (0.20, 0.10, 0.35, 0.10, 0.20),
         "Drinking Mountain Dew": (0.30, 0.10, 0.30, 0.20, 0.30),
         "Eating Doritos": (0.20, 0.10, 0.15, 0.15, 0.15),
         "Wearing Trenchcoat & Fedora": (0.10, 0.60, 0.10, 0.20, 0.15),
-    }, # All should vertically sum to 1
+    },  # All should vertically sum to 1
+    columns=("Eating Pizza", "Browsing Reddit", "Drinking Mountain Dew",
+             "Eating Doritos", "Wearing Trenchcoat & Fedora"),
     index=hidden_states)
 
-### Initialize starting probabilities
+# Initialize starting probabilities
 start_probs = DataFrame(
     data={"(0) {}".format(observations[0]): (0.10, 0.40, 0.10, 0.20, 0.20)},
     index=hidden_states)
 
-### Initialize forward dataframe
+# Initialize forward dataframe
 forward_df = start_probs.multiply(emit_prob_df[observations[0]], axis="index")
 colors_dict[forward_df.columns[0]] = next(colors)  # For final colored output
 
-### Start forward part - 1st pass
+# Start forward part - 1st pass
 for i, observation in enumerate(observations[1:]):  # Same as viterbi
     previous_forward_sum = trans_prob_df.iloc[:, :-1].multiply(
         forward_df.iloc[:, i], axis="index").sum()
@@ -51,18 +54,18 @@ for i, observation in enumerate(observations[1:]):  # Same as viterbi
         observation)] = previous_forward_sum * emit_prob_df.loc[:, observation]
     colors_dict[forward_df.columns[i + 1]] = next(colors)  # Update colors
 
-### Calculate forward probability
+# Calculate forward probability
 # Multiply last columns and sum the result
 forward_prob = (forward_df.iloc[:, -1] * trans_prob_df.iloc[:, -1]).sum()
 
-### Initialize backward dataframe
+# Initialize backward dataframe
 backward_df = DataFrame(
     data={  # The last column of trans_prob_df
         "({}) {}".format(len(observations) - 1, observations[-1]):
         trans_prob_df.iloc[:, -1]
     })
 
-### Start backward part - 2nd pass
+# Start backward part - 2nd pass
 for i, observation in zip(  # Same as viterbi
         range(len(observations) - 2, -1, -1), reversed(observations[1:])):
     backward_df.insert(  # Countdown to 2nd observation
@@ -72,20 +75,20 @@ for i, observation in zip(  # Same as viterbi
         (backward_df.iloc[:, 0] * trans_prob_df.iloc[:, :-1] *
          emit_prob_df.loc[:, observation]).sum(axis=1))  # Horizontal sum
 
-### Calculate backward probability: Should == forward probability
+# Calculate backward probability: Should == forward probability
 # Now use beginning values, opposite of forward
 backward_prob = (backward_df.iloc[:, 0] * start_probs.iloc[:, 0] *
                  emit_prob_df.loc[:, observations[0]]).sum()
 
-### Now merge the two - vectorized multiplication of all and divide by either
+# Now merge the two - vectorized multiplication of all and divide by either
 # forward or backward probability
-posterior_df = (forward_df * backward_df).apply(lambda x: x / forward_prob)
+posterior_df = (forward_df * backward_df) / forward_prob
 
-### Stylized output for reading top-down
+# Stylized output for reading top-down
 posterior_df_style = posterior_df.style.apply(  # Color the columns
     lambda x: ["background-color: {}".format(colors_dict[x.name])] * len(x))
 
-### Print final results - table should vertically sum to 1
+# Print final results - table should vertically sum to 1
 print("The observations:", ", ".join(observations))
 print("The most likely non-sequential hidden states are:")
 print(posterior_df.idxmax())
--- viterbi_pandas.py
+++ viterbi_pandas.py
@@ -1,14 +1,14 @@
 from pandas import DataFrame, IndexSlice
 from seaborn import light_palette
 
-### Initialize tuples of conditions.  Observations are the input
+# Initialize tuples of conditions.  Observations are the input
 observations = (  # Modify, add, remove with any key in emit_prob_df
     "Wearing Trenchcoat & Fedora", "Browsing Reddit", "Drinking Mountain Dew",
     "Eating Doritos", "Eating Pizza")
 hidden_states = ("Depressed", "Confident", "Tired", "Hungry",
                  "Thirsty")  # The confounding factors
 
-### Probability of transition
+# Probability of transition
 trans_prob_df = DataFrame(
     data={  # From depressed to confident, vice-versa, static, etc.
         "Depressed": (0.20, 0.25, 0.10, 0.20, 0.20),
@@ -17,28 +17,31 @@ trans_prob_df = DataFrame(
         "Hungry": (0.20, 0.20, 0.25, 0.30, 0.30),
         "Thirsty": (0.20, 0.20, 0.25, 0.20, 0.10)
     },  # All should vertically sum to 1
+    columns=("Depressed", "Confident", "Tired", "Hungry", "Thirsty"),
     index=hidden_states)
 
-### Probability of observation given the hidden state
+# Probability of observation given the hidden state
 emit_prob_df = DataFrame(
-    data={ # Highest chance of trenchcoat & fedora is when confident
+    data={  # Highest chance of trenchcoat & fedora is when confident
         "Eating Pizza": (0.20, 0.10, 0.10, 0.35, 0.20),
         "Browsing Reddit": (0.20, 0.10, 0.35, 0.10, 0.20),
         "Drinking Mountain Dew": (0.30, 0.10, 0.30, 0.20, 0.30),
         "Eating Doritos": (0.20, 0.10, 0.15, 0.15, 0.15),
         "Wearing Trenchcoat & Fedora": (0.10, 0.60, 0.10, 0.20, 0.15),
-    }, # All should vertically sum to 1
+    },  # All should vertically sum to 1
+    columns=("Eating Pizza", "Browsing Reddit", "Drinking Mountain Dew",
+             "Eating Doritos", "Wearing Trenchcoat & Fedora"),
     index=hidden_states)
 
-### Initialize starting probabilities
+# Initialize starting probabilities
 start_probs = DataFrame(
     data={"(0) {}".format(observations[0]): (0.10, 0.40, 0.10, 0.20, 0.20)},
     index=hidden_states)
 
-### Initialize dynammic programming matrix at probability 0
+# Initialize dynammic programming matrix at probability 0
 viterbi_df = start_probs.multiply(emit_prob_df[observations[0]], axis="index")
 
-### Start dynammic programming
+# Start dynammic programming
 for i, observation in enumerate(observations[1:]):
     max_trans_prob_df = trans_prob_df.multiply(  # Offset by 1
         viterbi_df.iloc[:, i], axis="index").max()
@@ -48,21 +51,21 @@ for i, observation in enumerate(observations[1:]):
         i + 1,  # Then multiply the result by the observation emissions
         observation)] = max_trans_prob_df * emit_prob_df.loc[:, observation]
 
-### Provide the entire matrix with highest values darkest
+# Provide the entire matrix with highest values darkest
 viterbi_traceback_df = viterbi_df.style.background_gradient(
     cmap=light_palette("green", as_cmap=True))
 
-### At the last column, use the maximum value to begin traceback
+# At the last column, use the maximum value to begin traceback
 traceback_prob = [viterbi_df.iloc[:, -1].max()]
 dyn_prog_path = [viterbi_df.iloc[:, -1].idxmax()]  # And its index
 viterbi_traceback_df.highlight_max(  # Highlight it
     color="red", subset=IndexSlice[[viterbi_df.columns[-1]]])
 
-### Start traceback
+# Start traceback
 for i, observation in zip(  # Reverse enumerate with offset
         range(len(observations) - 2, -1, -1), reversed(observations[1:])):
     # Isolate the previous location that gives the current probability
-    traceback_loc = viterbi_df.loc[ # Always going left-most
+    traceback_loc = viterbi_df.loc[  # Always going left-most
         viterbi_df.iloc[:, i] * trans_prob_df.loc[:, dyn_prog_path[0]] *
         emit_prob_df.loc[dyn_prog_path[0], observation] == traceback_prob[
             0]].index[0]
@@ -74,9 +77,9 @@ for i, observation in zip(  # Reverse enumerate with offset
         lambda x: "background-color: red",  # Color the path red
         subset=IndexSlice[[dyn_prog_path[0]], [viterbi_df.columns[i]]])
 
-### Print dynammic programming matrix and traceback results
+# Print dynammic programming matrix and traceback results
 print("The observations:", ", ".join(observations))
 print("The most likely sequence of hidden states is:")
-print((viterbi_df.isin(traceback_prob) == True).idxmax())
+print((viterbi_df.isin(traceback_prob)).idxmax())
 print("The final probability:", traceback_prob[-1])
 viterbi_traceback_df
